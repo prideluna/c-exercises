59e99c996c88f4823a856b7d6e135e6f079c9eec
Priscila Luna <74717489+prideluna@users.noreply.github.com>
2022-03-08 16:46:34 -0300

    Create printingNameFamilyName.c

---
+++ printingNameFamilyName.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main ()
+{
+    char name[100];
+    char familyName[100];
+    printf("Enter your name: ");
+    gets(nome);
+    printf("Enter your family name: ");
+    gets(familyName);
+    printf("%s,%s" ,familyName, name);
+  
+}
